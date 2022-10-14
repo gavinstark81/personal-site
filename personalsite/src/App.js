63f9167bf2621f890278f1499471d1dff9ce79ff
gavinstark81 <106928307+gavinstark81@users.noreply.github.com>
2022-10-13 21:35:09 -0500

    routes not working but can make components render

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -1,3 +1,16 @@
-export default function App() {
-  return <></>
+import { Route, Routes } from "react-router-dom";
+import  Home  from "./components/Home"
+
+function App() {
+  return (
+    <div id="appDiv">
+      {/* <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes> */}
+    <h1>Hello World</h1>
+    <Home />
+    </div>
+  );
 }
+
+export default App;
---
+++ personalsite/src/components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+        <h1>THIS IS MY HOME PAGE</h1>
+        <button>Home</button>
+        </>
+    );
+}
+
